01e7bb67aa9c23705b47a4cffdeb2657131e3ada
abhishekgole004 <abhishekgole19@gmail.com>
2026-04-19 21:09:34 +0530

    Added logging feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 def greet():
-    return "Hello from DevOps App"
+    return "Hello from DevOps App with Logging"
 
 if __name__ == "__main__":
+    print("[LOG] Starting app...")
     print(greet())
